c7c04641a971c81c6893e7819acc2983aef889bb
Sheldon Hearn <sheldonh@starjuice.net>
2014-10-10 09:52:05 +0200

    Just enough for now

--- redis/skydns-discovery/discover.rb
+++ redis/skydns-discovery/discover.rb
@@ -46,7 +46,26 @@ if ARGV.size < 1
   exit 1
 end
 
-ARGV.each do |name|
-  search = resolver.search(name, Net::DNS::SRV)
-  p search.elements
+class Answer
+  attr_reader :address, :port
+
+  def initialize(address: nil, port: nil)
+    @address, @port = address, port
+  end
+
+  def to_s
+    "#{address}:#{port}"
+  end
 end
+
+answers = ARGV.inject([]) do |acc, name|
+  packet = resolver.search(name, Net::DNS::SRV)
+  addresses = packet.additional.select { |rr| rr.type == "A" }.inject({}) { |m, a| m[a.name] = a.address.to_s; m }
+  packet.answer.each do |rr|
+    address = addresses[rr.host]
+    acc << Answer.new(address: address, port: rr.port)
+  end
+  acc
+end
+
+puts answers.shuffle.join(" ")
